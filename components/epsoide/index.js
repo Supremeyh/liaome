e88b75e08f60144aaf7294ff8073bab52bfe504c
supremeyh <supremeyh@126.com>
2018-11-04 00:24:51 +0800

    11.4 epsoide页面

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -22,16 +22,25 @@ Component({
    * 组件的初始数据
    */
   data: {
+    months: [
+      '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一              月', '十一月'],
     year:  0,
     month: '',
     _index: ''
-
 
   },
 
   attached: function(){
     // console.log(this.properties)
     // console.log(this.data)
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
+
+    this.setData({
+      year: year,
+      month: this.month[month]
+    })
   },
 
   /**
